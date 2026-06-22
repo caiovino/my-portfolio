78a18b38895068eb1ab81c73a7b5792b93fb2852
Luigi Salemi <lsalemi@tiscali.it>
2026-06-22 16:05:30 -0400

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 export default function Home() {
 
+
+
   return(
-    <main>
-      <h1> LineSquare Portfolio</h1>
-      <p> coming soon...</p>
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5x1 font-bold text-center shadow-lg"> 
+        LineSquare Portfolio
+        </h1>
+      <p className="mt-4 text-x1"> 
+        coming soon...
+        </p>
     </main>
   )
 }
