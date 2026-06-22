9395be9bd4fed0facc065dcfa91364eb4464f6f4
Luigi Salemi <lsalemi@tiscali.it>
2026-06-22 18:23:08 -0400

    "Iteration 0.0"

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
 
 
   return(
-    <main className="min-h-screen bg-cyan-900 text-indigo-100 flex pretty items-center justify-center">
-      <h1 className="text-9x1 font-bold text-center shadow-lg"> 
+    <main className="min-h-screen bg-cyan-900 text-indigo-100  flex pretty ">
+      <h1 className="8 text-9x1 font-bold text-center align-top wavy shadow-lg"> 
         LineSquare Portfolio
         </h1>
       <p className="mt-4 text-x1"> 
