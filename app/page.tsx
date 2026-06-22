48956c1f8a2ead32a1bcefdd7cf8c2b6d3319008
Luigi Salemi <lsalemi@tiscali.it>
2026-06-22 16:38:04 -0400

    "Initial Commit"

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
 
 
   return(
-    <main className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-5x1 font-bold text-center shadow-lg"> 
+    <main className="min-h-screen bg-cyan-900 text-indigo-100 flex pretty items-center justify-center">
+      <h1 className="text-9x1 font-bold text-center shadow-lg"> 
         LineSquare Portfolio
         </h1>
       <p className="mt-4 text-x1"> 
